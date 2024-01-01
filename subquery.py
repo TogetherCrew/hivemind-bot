ede353c36147e497495739386bd2ef30a2c91f80
Mohammad Amin <dadgaramin96@gmail.com>
2024-01-01 14:43:39 +0330

    feat: Added subquery reponse generator!

---
+++ subquery.py
@@ -0,0 +1,104 @@
+from utils.query_engine import prepare_discord_engine_auto_filter
+from llama_index.core import BaseQueryEngine
+from guidance.llms import OpenAI as GuidanceOpenAI
+from llama_index import QueryBundle
+from llama_index.tools import QueryEngineTool, ToolMetadata
+from llama_index.query_engine import SubQuestionQueryEngine
+from llama_index.question_gen.guidance_generator import GuidanceQuestionGenerator
+
+def query_multiple_source(
+        query: str,
+        community_id: str,
+        discord: bool,
+        discourse: bool,
+        gdrive: bool,
+        notion: bool,
+        telegram: bool,
+        github: bool,
+    ) -> str:
+    """
+    query multiple platforms and get an answer from the multiple
+
+    Parameters
+    ------------
+    query : str
+        the user question
+    community_id : str
+        the community id to get their data
+    discord : bool
+        if `True` then add the engine to the subquery_generator
+    discourse : bool
+        if `True` then add the engine to the subquery_generator
+    gdrive : bool
+        if `True` then add the engine to the subquery_generator
+    notion : bool
+        if `True` then add the engine to the subquery_generator
+    telegram : bool
+        if `True` then add the engine to the subquery_generator
+    github : bool
+        if `True` then add the engine to the subquery_generator
+
+
+    Returns
+    --------
+    reponse : str
+        the response to the user query from the LLM
+        using the engines of the given platforms (pltform equal to True)
+    """
+    query_engine_tools: list[QueryEngineTool] = []
+    tools: list[ToolMetadata] = []
+
+    discord_query_engine: BaseQueryEngine
+    discourse_query_engine: BaseQueryEngine
+    gdrive_query_engine: BaseQueryEngine
+    notion_query_engine: BaseQueryEngine
+    telegram_query_engine: BaseQueryEngine
+    github_query_engine: BaseQueryEngine
+
+    # query engine perparation
+    # tools_metadata and query_engine_tools
+    if discord:
+        discord_query_engine = prepare_discord_engine_auto_filter(
+            community_id,
+            query,
+            similarity_top_k=None,
+            d=None,
+        )
+        tool_metadata = ToolMetadata(
+            name="Discord",
+            description="Provides the discord platform conversations data."
+        )
+
+        tools.append(tool_metadata)
+        query_engine_tools.append(
+            QueryEngineTool(
+                query_engine=discord_query_engine,
+                metadata=tool_metadata,
+            )
+        )
+    
+    if discourse:
+        raise NotImplementedError
+    if gdrive:
+        raise NotImplementedError
+    if notion:
+        raise NotImplementedError
+    if telegram:
+        raise NotImplementedError
+    if github:
+        raise NotImplementedError
+    
+
+    question_gen = GuidanceQuestionGenerator.from_defaults(
+        guidance_llm=GuidanceOpenAI("text-davinci-003"), verbose=False
+    )
+
+    s_engine = SubQuestionQueryEngine.from_defaults(
+        question_gen=question_gen,
+        query_engine_tools=query_engine_tools,
+    )
+    reponse = s_engine.query(
+        QueryBundle(query)
+    )
+
+    return reponse.response
---
+++ utils/query_engine/discord_query_engine.py
@@ -0,0 +1,136 @@
+from bot.retrievers.forum_summary_retriever import ForumBasedSummaryRetriever
+from bot.retrievers.process_dates import process_dates
+from bot.retrievers.utils.load_hyperparams import load_hyperparams
+from llama_index.core import BaseQueryEngine
+from llama_index.vector_stores import ExactMatchFilter, FilterCondition, MetadataFilters
+from tc_hivemind_backend.pg_vector_access import PGVectorAccess
+
+
+def prepare_discord_engine(
+    community_id: str,
+    thread_names: list[str],
+    channel_names: list[str],
+    days: list[str],
+    similarity_top_k: int | None = None,
+) -> BaseQueryEngine:
+    """
+    query the discord database using filters given
+    and give an anwer to the given query using the LLM
+
+    Parameters
+    ------------
+    guild_id : str
+        the discord guild data to query
+    query : str
+        the query (question) of the user
+    thread_names : list[str]
+        the given threads to search for
+    channel_names : list[str]
+        the given channels to search for
+    days : list[str]
+        the given days to search for
+    similarity_top_k : int | None
+        the k similar results to use when querying the data
+        if `None` will load from `.env` file
+
+    Returns
+    ---------
+    query_engine : BaseQueryEngine
+        the created query engine with the filters
+    """
+    table_name = "discord"
+    dbname = f"community_{community_id}"
+
+    pg_vector = PGVectorAccess(table_name=table_name, dbname=dbname)
+    index = pg_vector.load_index()
+    if similarity_top_k is None:
+        _, similarity_top_k, _ = load_hyperparams()
+
+    thread_filters: list[ExactMatchFilter] = []
+    channel_filters: list[ExactMatchFilter] = []
+    day_filters: list[ExactMatchFilter] = []
+
+    for channel in channel_names:
+        channel_updated = channel.replace("'", "''")
+        channel_filters.append(ExactMatchFilter(key="channel", value=channel_updated))
+
+    for thread in thread_names:
+        thread_updated = thread.replace("'", "''")
+        thread_filters.append(ExactMatchFilter(key="thread", value=thread_updated))
+
+    for day in days:
+        day_filters.append(ExactMatchFilter(key="date", value=day))
+
+    all_filters: list[ExactMatchFilter] = []
+    all_filters.extend(thread_filters)
+    all_filters.extend(channel_filters)
+    all_filters.extend(day_filters)
+
+    filters = MetadataFilters(filters=all_filters, condition=FilterCondition.OR)
+
+    query_engine = index.as_query_engine(
+        filters=filters, similarity_top_k=similarity_top_k
+    )
+
+    return query_engine
+
+
+def prepare_discord_engine_auto_filter(
+    community_id: str,
+    query: str,
+    similarity_top_k: int | None = None,
+    d: int | None = None,
+) -> BaseQueryEngine:
+    """
+    get the query engine and do the filtering automatically.
+    By automatically we mean, it would first query the summaries
+    to get the metadata filters
+
+    Parameters
+    -----------
+    guild_id : str
+        the discord guild data to query
+    query : str
+        the query (question) of the user
+    similarity_top_k : int | None
+        the value for the initial summary search
+        to get the `k2` count simliar nodes
+        if `None`, then would read from `.env`
+    d : int
+        this would make the secondary search (`prepare_discord_engine`)
+        to be done on the `metadata.date - d` to `metadata.date + d`
+
+
+    Returns
+    ---------
+    query_engine : BaseQueryEngine
+        the created query engine with the filters
+    """
+    table_name = "discord_summary"
+    dbname = f"community_{community_id}"
+
+    if d is None:
+        _, _, d = load_hyperparams()
+    if similarity_top_k is None:
+        similarity_top_k, _, _ = load_hyperparams()
+
+    discord_retriever = ForumBasedSummaryRetriever(table_name=table_name, dbname=dbname)
+
+    channels, threads, dates = discord_retriever.retreive_metadata(
+        query=query,
+        metadata_group1_key="channel",
+        metadata_group2_key="thread",
+        metadata_date_key="date",
+        similarity_top_k=similarity_top_k,
+    )
+
+    dates_modified = process_dates(list(dates), d)
+
+    engine = prepare_discord_engine(
+        community_id=community_id,
+        query=query,
+        thread_names=list(threads),
+        channel_names=list(channels),
+        days=dates_modified,
+    )
+    return engine
