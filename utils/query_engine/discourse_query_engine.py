96da09668f1898ed2e4a03f0a890059254ce8d75
Mohammad Amin <dadgaramin96@gmail.com>
2024-01-16 15:08:46 +0330

    update: saving the codes. We still need to confirm it is working right and test cases should be written for the changes.

--- utils/query_engine/discourse_query_engine.py
+++ utils/query_engine/discourse_query_engine.py
@@ -2,33 +2,33 @@ from llama_index.query_engine import BaseQueryEngine
 from .level_based_platform_query_engine import LevelBasedPlatformQueryEngine
 
 
-def prepare_discord_engine(
+def prepare_discourse_engine(
     community_id: str,
-    thread_names: list[str],
-    channel_names: list[str],
+    category_names: list[str],
+    topic_names: list[str],
     days: list[str],
     **kwarg,
 ) -> BaseQueryEngine:
     """
-    query the platform database using filters given
+    query the discourse database using filters given
     and give an anwer to the given query using the LLM
 
     Parameters
     ------------
     community_id : str
-        the discord community id data to query
+        the discourse community data to query
     query : str
         the query (question) of the user
-    level1_names : list[str]
+    category_names : list[str]
         the given categorys to search for
-    level2_names : list[str]
+    topic_names : list[str]
         the given topics to search for
     days : list[str]
         the given days to search for
+    similarity_top_k : int | None
+        the k similar results to use when querying the data
+        if `None` will load from `.env` file
     ** kwargs :
-        similarity_top_k : int | None
-            the k similar results to use when querying the data
-            if not given, will load from `.env` file
         testing : bool
             whether to setup the PGVectorAccess in testing mode
 
@@ -37,18 +37,20 @@ def prepare_discord_engine(
     query_engine : BaseQueryEngine
         the created query engine with the filters
     """
-    query_engine_preparation = get_discord_level_based_platform_query_engine()
-    query_engine = query_engine_preparation.prepare_platform_engine(
+    level_based_query_engine = get_discourse_level_based_platform_query_engine()
+
+    query_engine = level_based_query_engine.prepare_platform_engine(
         community_id=community_id,
-        level1_names=thread_names,
-        level2_names=channel_names,
+        level1_names=category_names,
+        level2_names=topic_names,
         days=days,
         **kwarg,
     )
+
     return query_engine
 
 
-def prepare_discord_engine_auto_filter(
+def prepare_discourse_engine_auto_filter(
     community_id: str,
     query: str,
     similarity_top_k: int | None = None,
@@ -61,8 +63,8 @@ def prepare_discord_engine_auto_filter(
 
     Parameters
     -----------
-    community_id : str
-        the discord community data to query
+    guild_id : str
+        the discourse guild data to query
     query : str
         the query (question) of the user
     similarity_top_k : int | None
@@ -70,31 +72,31 @@ def prepare_discord_engine_auto_filter(
         to get the `k2` count simliar nodes
         if `None`, then would read from `.env`
     d : int
-        this would make the secondary search (`prepare_discord_engine`)
+        this would make the secondary search (`prepare_discourse_engine`)
         to be done on the `metadata.date - d` to `metadata.date + d`
-
 
     Returns
     ---------
     query_engine : BaseQueryEngine
         the created query engine with the filters
     """
+    level_based_query_engine = get_discourse_level_based_platform_query_engine()
 
-    query_engine_preparation = get_discord_level_based_platform_query_engine()
-    query_engine = query_engine_preparation.prepare_engine_auto_filter(
+    query_engine = level_based_query_engine.prepare_engine_auto_filter(
         community_id=community_id,
         query=query,
         similarity_top_k=similarity_top_k,
         d=d,
     )
-
     return query_engine
 
 
-def get_discord_level_based_platform_query_engine() -> LevelBasedPlatformQueryEngine:
+def get_discourse_level_based_platform_query_engine() -> LevelBasedPlatformQueryEngine:
+    """
+    just perpare the `LevelBasedPlatformQueryEngine` to use
+
+    """
     level_based_query_engine = LevelBasedPlatformQueryEngine(
-        level1_key="thread",
-        level2_key="channel",
-        platform_table_name="discord",
+        level1_key="category", level2_key="topic", platform_table_name="discourse"
     )
     return level_based_query_engine
