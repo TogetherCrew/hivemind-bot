3f6363cae955dd71abe6dea5b919ae4b23331705
Mohammad Amin <dadgaramin96@gmail.com>
2024-01-22 16:56:22 +0330

    fix: isort linter issue!

--- celery_app/tasks.py
+++ celery_app/tasks.py
@@ -4,7 +4,6 @@ from typing import Any
 
 from celery_app.server import app
 from celery_app.utils.fire_event import job_send
-from traceloop.sdk import Traceloop
 from subquery import query_multiple_source
 from tc_messageBroker.rabbit_mq.event import Event
 from tc_messageBroker.rabbit_mq.payload.discord_bot.base_types.interaction_callback_data import (
@@ -15,6 +14,7 @@ from tc_messageBroker.rabbit_mq.payload.discord_bot.chat_input_interaction impor
 )
 from tc_messageBroker.rabbit_mq.payload.payload import Payload
 from tc_messageBroker.rabbit_mq.queue import Queue
+from traceloop.sdk import Traceloop
 
 
 @app.task
