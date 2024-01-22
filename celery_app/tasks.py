d32151cda2c9fc8118583e0657195fdaf7b7bac8
Mohammad Amin <dadgaramin96@gmail.com>
2024-01-22 13:03:30 +0330

    fix: isort linter issue!

--- celery_app/tasks.py
+++ celery_app/tasks.py
@@ -1,11 +1,11 @@
 import json
 import logging
 import os
-from dotenv import load_dotenv
 from typing import Any
 
 from celery_app.server import app
 from celery_app.utils.fire_event import job_send
+from dotenv import load_dotenv
 from subquery import query_multiple_source
 from tc_messageBroker.rabbit_mq.event import Event
 from tc_messageBroker.rabbit_mq.payload.discord_bot.base_types.interaction_callback_data import (
