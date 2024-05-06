1f5a86aa8b4751535b78b548931c15dde953ce8f
Mohammad Amin <dadgaramin96@gmail.com>
2024-05-06 12:53:02 +0330

    fix: isort linter issue!

--- utils/query_engine/__init__.py
+++ utils/query_engine/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
-from .github import GitHubQueryEngine
 from .gdrive import GDriveQueryEngine
+from .github import GitHubQueryEngine
 from .prepare_discord_query_engine import prepare_discord_engine_auto_filter
 from .subquery_gen_prompt import DEFAULT_GUIDANCE_SUB_QUESTION_PROMPT_TMPL
