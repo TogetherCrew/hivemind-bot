f77013a6cde20ca67c3f613b2bf780d084b3a7e1
Mohammad Amin <dadgaramin96@gmail.com>
2024-05-06 12:27:22 +0330

    feat: Added google drive RAG pipeline!

--- utils/query_engine/__init__.py
+++ utils/query_engine/__init__.py
@@ -1,4 +1,5 @@
 # flake8: noqa
 from .github import GitHubQueryEngine
+from .gdrive import GDriveQueryEngine
 from .prepare_discord_query_engine import prepare_discord_engine_auto_filter
 from .subquery_gen_prompt import DEFAULT_GUIDANCE_SUB_QUESTION_PROMPT_TMPL
