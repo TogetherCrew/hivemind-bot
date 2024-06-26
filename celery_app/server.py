16bc35556932992b9e18f59c400451d52eb442d2
Mohammad Amin <dadgaramin96@gmail.com>
2024-06-26 18:28:47 +0330

    feat: move back to pyamqp!

--- celery_app/server.py
+++ celery_app/server.py
@@ -8,6 +8,6 @@ host = rabbit_creds["host"]
 port = rabbit_creds["port"]
 
 app = Celery(
-    "celery_app/tasks", broker=f"librabbitmq://{user}:{password}@{host}:{port}//"
+    "celery_app/tasks", broker=f"pyamqp://{user}:{password}@{host}:{port}//"
 )
 app.autodiscover_tasks(["celery_app"])
