dca3c1cadd9e3a015b7c6be107ab1e368e9564a3
Mohammad Amin <dadgaramin96@gmail.com>
2024-09-23 14:46:26 +0330

    feat: RabbitMQRouter aligned with the hivemind agnostic! + still http API is todo.

--- routers/amqp.py
+++ routers/amqp.py
@@ -1,7 +1,10 @@
-from fastapi import Depends
+from datetime import datetime
+
 from pydantic import BaseModel
 from faststream.rabbit.fastapi import RabbitRouter, Logger  # type: ignore
+from faststream.rabbit.schemas.queue import RabbitQueue
 from utils.credentials import load_rabbitmq_credentials
+from tc_messageBroker.rabbit_mq.queue import Queue
 from tc_messageBroker.rabbit_mq.event import Event
 from worker.tasks import query_data_sources
 
@@ -10,19 +13,36 @@ rabbitmq_creds = load_rabbitmq_credentials()
 router = RabbitRouter(rabbitmq_creds["url"])
 
 
-class Incoming(BaseModel):
+class Content(BaseModel):
     question: str
     community_id: str
 
 
-def call():
-    return True
+class Payload(BaseModel):
+    event: str
+    date: datetime | str
+    content: Content | dict
 
 
-@router.subscriber(Event.HIVEMIND.INTERACTION_CREATED)
-@router.publisher(Event.DISCORD_BOT.INTERACTION_RESPONSE.EDIT)
-async def ask(m: Incoming, logger: Logger, d=Depends(call)):
-    logger.info(f"COMMUNITY_ID: {m.community_id} Received job")
-    response = query_data_sources(community_id=m.community_id, query=m.question)
-    # TODO: find out how the publishing to DISCORD_BOT queue works
-    return response
+@router.subscriber(queue=RabbitQueue(name=Queue.HIVEMIND, durable=True))
+@router.publisher(queue=RabbitQueue(Queue.DISCORD_BOT, durable=True))
+async def ask(payload: Payload, logger: Logger):
+    if payload.event == Event.HIVEMIND.INTERACTION_CREATED:
+        question = payload.content.question
+        community_id = payload.content.community_id
+
+        logger.info(f"COMMUNITY_ID: {community_id} Received job")
+        response = query_data_sources(community_id=community_id, query=question)
+        logger.info(f"COMMUNITY_ID: {community_id} Job finished")
+
+        response_payload = Payload(
+            event=Event.DISCORD_BOT.INTERACTION_RESPONSE.EDIT,
+            date=str(datetime.now()),
+            content={"response": response},
+        )
+    else:
+        raise NotImplementedError(
+            f"No more event available for {Queue.HIVEMIND} queue! "
+            f"Received event: `{payload.event}`"
+        )
+    return response_payload
