8a32cc7f2dff0971bb404345243dd4fa39309f4c
Mohammad Amin <dadgaramin96@gmail.com>
2025-07-20 15:38:39 +0330

    feat: update response while no context knowledge is available!

--- utils/query_engine/qa_prompt.py
+++ utils/query_engine/qa_prompt.py
@@ -1,4 +1,5 @@
 from llama_index.core import PromptTemplate
+from utils.globals import NO_ANSWER_REFERENCE
 
 qa_prompt = PromptTemplate(
     "Context information is below.\n"
@@ -7,6 +8,8 @@ qa_prompt = PromptTemplate(
     "---------------------\n"
     "Given the context information and not prior knowledge, "
     "give a short answer to the query.\n"
+    "If the query is not related to the context, respond with "
+    f"'{NO_ANSWER_REFERENCE}'\n"
     "Query: {query_str}\n"
     "Answer (concisely): "
 )
